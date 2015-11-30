768743456f0630ac52bfb1ac4ca84356faf6e8dc
Winfred Zhu <winfredzhu@outlook.com>
2015-11-30 06:20:45 +0000

    Add parser structure

--- snippets.py
+++ snippets.py
@@ -1,19 +1,31 @@
+import psycopg2 
 import logging
 
 logging.basicConfig(filename='snippets.log',level=logging.DEBUG)
 import argparse
 import sys
 import os
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
 
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+
+def connect_to_db():
+    logging.debug("Connecting to PostgreSQL")
+    connection = psycopg2.connect(database="snippets")
+    logging.debug("Database connection established.")
+
+
+def put(name, snippet):
+
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")   
     return name, snippet
     
     
+
 def get(name):
     """Retrieve the snippet with a given name.
 
@@ -28,10 +40,30 @@ def get(name):
     
 def main():
     
+    """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    print sys.argv[1:]
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    get_paser=subparsers.add_parser("get",help="Retrieve a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    get_paser.add_argument("name",help="The name of the snippet")
+
     arguments = parser.parse_args(sys.argv[1:])
+    
+    arguments = vars(arguments)
+    command=arguments.pop("command")
+    if command=="put":
+        name,snippet=put(**arguments)
+        print "Stored {!r} as {!r}".format(snippet,name)
+    elif command=="get":
+        snippet=get(**arguments)
+        print "Retrieve snippet: {!r}".format(snippet)
 
 if __name__ == '__main__':
     main()
